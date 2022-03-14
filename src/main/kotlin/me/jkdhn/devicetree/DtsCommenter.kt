0e497398e3fb070bf3c569c202850a0975ca88a4
Jack Dähn <jack@jkdhn.me>
2022-03-14 16:23:53 +0100

    Add commenter

---
+++ src/main/kotlin/me/jkdhn/devicetree/DtsCommenter.kt
@@ -0,0 +1,15 @@
+package me.jkdhn.devicetree
+
+import com.intellij.lang.Commenter
+
+class DtsCommenter : Commenter {
+    override fun getLineCommentPrefix() = "//"
+
+    override fun getBlockCommentPrefix() = "/*"
+
+    override fun getBlockCommentSuffix() = "*/"
+
+    override fun getCommentedBlockCommentPrefix() = null
+
+    override fun getCommentedBlockCommentSuffix() = null
+}
